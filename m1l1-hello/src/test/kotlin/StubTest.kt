9f7c2d5e8cdc3eef4c9e60b621889a32ea882929
Alexander Porublev <porublevav@gmail.com>
2023-02-28 17:38:12 +0300

    m1l1-hello

---
+++ m1l1-hello/src/test/kotlin/StubTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+class StubTest {
+
+    @Test
+    fun test(){
+        assertEquals(4, 2*2)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+
+rootProject.name = "ok-marketplace"
+include("m1l1-hello")
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins{
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
